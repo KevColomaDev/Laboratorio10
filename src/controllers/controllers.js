4501a4c0fe375577992122eb4c50860cc871a0ba
Kevin <kev.enrri@hotmail.com>
2024-06-05 14:29:16 -0500

    All functions

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,25 +1,56 @@
 import { v4 } from 'uuid'
-import { testModel } from '../models/models.js'
+import { modelHole } from '../models/models.js'
 
-export const getAllTestController = async (req, res) => {
+export const getAllHoleController = async (req, res) => {
   try {
-    const dataTest = await testModel.getAllTestModel()
+    const dataTest = await modelHole.getAllTestModel()
     res.status(200).json(dataTest)
   } catch (error) {
-    res.status(500).json({ msg: `Mensaje de error: ${error}` })
+    res.status(500).json({ msg: error })
   }
 }
 
-export const createDataTestController = async (req, res) => {
+export const createHoleController = async (req, res) => {
   try {
-    const newDataTest = {
+    const newDataHole = {
       id: v4(),
       ...req.body
     }
-    const result = await testModel.createDataTestModel(newDataTest)
+    const result = await modelHole.createHole(newDataHole)
     res.status(200).json(result)
   } catch (error) {
     res.status(500).json({ msg: error })
     console.log(error)
+  }
+}
+
+export const getHoleByIdController = async (req, res) => {
+  try {
+    const id = req.params.id
+    const result = await modelHole.getHoleById(id)
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+}
+
+export const updateHoleController = async (req, res) => {
+  try {
+    const id = req.params.id
+    const newData = req.body
+    const result = await modelHole.updateHole(id, newData)
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+}
+
+export const deleteHoleController = async (req, res) => {
+  try {
+    const id = req.params.id
+    const result = await modelHole.deleteHole(id)
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(500).json({ msg: error })
   }
 }
--- src/models/models.js
+++ src/models/models.js
@@ -1,12 +1,32 @@
-export const testModel = {
-  async getAllTestModel () {
+export const modelHole = {
+  async getAllHole () {
     const peticion = await fetch('http://localhost:4000/test')
     return peticion.json()
   },
-  async createDataTestModel (newData) {
+  async createHole (newData) {
     const url = 'http://localhost:4000/test'
     const peticion = await fetch(url, {
       method: 'POST',
+      body: JSON.stringify(newData),
+      headers: { 'Content-type': 'application/json' }
+    })
+    return peticion.json()
+  },
+  async getHoleById (id) {
+    const peticion = await fetch(`http://localhost:4000/test/${id}`)
+    return peticion.json()
+  },
+  async deleteHole (id) {
+    const url = `http://localhost:4000/test/${id}`
+    const peticion = await fetch(url, {
+      method: 'DELETE'
+    })
+    return peticion.json()
+  },
+  async updateHole (id, newData) {
+    const url = `http://localhost:4000/test/${id}`
+    const peticion = await fetch(url, {
+      method: 'PUT',
       body: JSON.stringify(newData),
       headers: { 'Content-type': 'application/json' }
     })
