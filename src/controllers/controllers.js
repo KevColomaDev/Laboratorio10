53f031f023ec4e0f9c37446d8700f9ca65362ade
Kevin <kev.enrri@hotmail.com>
2024-06-05 00:03:07 -0500

    Nuevo Test 1

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -6,7 +6,7 @@ export const getAllTestController = async (req, res) => {
     const dataTest = await testModel.getAllTestModel()
     res.status(200).json(dataTest)
   } catch (error) {
-    res.status(500).json({ msg: `Mensaje: ${error}` })
+    res.status(500).json({ msg: `Mensaje de error: ${error}` })
   }
 }
 
